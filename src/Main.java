fe9b4ff1584d8be205895cfe5b90024276bc333d
Baronas0927 <do.baublys@gmail.com>
2024-05-21 14:43:08 +0300

    1 . Sukurkite Funkciją kuri priima du int tipo kintamuosius. Juos susumuoja ir atspausdina. 2. Sukurkite Funkciją kuri vadinasi PISq. Funkcija gražina double tipo reikšmę. Reikšmė yra : 9.8596; Gautą reikšmę atspausdinkite. 3. Sukurkite Funkciją kuri priima du int tipo kintamuosius. Funkcija gražina skaičių sandaugą.; Gautą reikšmę atspausdinkite.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        
-        System.out.println("Hello world!");
+        System.out.println("======= 1 funkcija =======");
+        priimti();
+        System.out.println("======= 2 funkcija =======");
+    pisQ();
+        System.out.println("======= 3 funkcija =======");
+    sandauga();
+
+
+
+    }
+    public static void priimti(){
+        int sk = 1;
+        int sk1 = 2;
+                    int sum = sk + sk1;
+        System.out.println(sum);
+    }
+    public static void pisQ(){
+        double d= 9.8596;
+        System.out.println(d);
+    }
+    public static void sandauga(){
+        int num= 3;
+        int num1= 4;
+        int x = num * num1;
+        System.out.println(x);
     }
 }
